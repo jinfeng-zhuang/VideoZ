a5d089abd3fc0afa7d279e52c11048ba2d0ce58a
jinfeng <jinfeng.zhuang@aliyun.com>
2018-07-08 15:35:00 +0800

    jpeg: trans jpg to bmp

--- JPEG/jpeg_section.h
+++ JPEG/jpeg_section.h
@@ -100,6 +100,29 @@ struct jpeg_marker_sof {
 		uint8_t quanid;
 	} color_component_array[3]; // DOC, to ARRAY[1]
 };
+
+struct bmp_file_header {
+	uint16_t type;
+	uint32_t size;
+	uint16_t rsvd1;
+	uint16_t rsvd2;
+	uint32_t rgb_offset;
+};
+
+struct bmp_image_header {
+	uint32_t header_size;
+	uint32_t width;
+	uint32_t height;
+	uint16_t planes;
+	uint16_t bits_per_pixel;
+	uint32_t compress;
+	uint32_t size;
+	uint32_t xpixel_per_meter;
+	uint32_t ypixel_per_meter;
+	uint32_t color_used;
+	uint32_t important_color;
+};
+
 #pragma pack(pop)
 
 #define BIGENDIAN_16(x) (((x>>8)&0xFF) | ((x&0xFF)<<8))
--- JPEG/main.cpp
+++ JPEG/main.cpp
@@ -5,21 +5,96 @@
 
 #include "jpeg_section.h"
 
-FILE *fp;
+#define FRAME_WIDTH		(128)
+#define FRAME_HEIGHT	(128)
+#define FRAME_SIZE		(FRAME_WIDTH * FRAME_HEIGHT * 3)
+uint8_t frame[FRAME_SIZE] = { 0 };
 
-// 16 x 16 x 3 => RGB 16 x 16 pixels
-int output(uint8_t *matrix)
+int write_bmp_header(FILE *fp)
 {
-	assert(NULL != matrix);
 	assert(NULL != fp);
 
 	int ret;
 
-	ret = fwrite(matrix, 1, 16 * 16 * 3, fp);
-	assert(ret == 16 * 16 * 3);
+	struct bmp_file_header file;
+	struct bmp_image_header image;
 
-	ret = fflush(fp);
-	assert(0 == ret);
+	memset(&file, 0, sizeof(struct bmp_file_header));
+	memset(&image, 0, sizeof(struct bmp_image_header));
+
+	file.type = 'MB';
+	file.size = sizeof(file) + sizeof(image) + FRAME_SIZE;
+	file.rgb_offset = sizeof(file) + sizeof(image);
+
+	image.header_size = sizeof(image);
+	image.width = FRAME_WIDTH;
+	image.height = FRAME_HEIGHT;
+	image.planes = 1;
+	image.bits_per_pixel = 24;
+	image.compress = 0;
+
+	ret = fwrite(&file, 1, sizeof(file), fp);
+	assert(ret == sizeof(file));
+
+	ret = fwrite(&image, 1, sizeof(image), fp);
+	assert(ret == sizeof(image));
+
+	return 0;
+}
+
+// assume
+// put 16x16 block to a container
+// unit = 3
+// fw is 16xN
+// fh is 16xN
+int output(uint8_t *matrix)
+{
+	assert(NULL != matrix);
+	assert(NULL != frame);
+	
+	assert(FRAME_WIDTH % 16 == 0);
+	assert(FRAME_HEIGHT % 16 == 0);
+
+#define BLOCK_WIDTH		(16)
+#define BLOCK_HEIGHT	(16)
+#define UNIT_SIZE		(3)
+
+	static int x=0, y=0;
+
+	int base = y * FRAME_WIDTH + x;
+
+	for (int row = 0; row < BLOCK_HEIGHT; row++) {
+		for (int col = 0; col < BLOCK_WIDTH; col++) {
+			frame[(base + row * FRAME_WIDTH + col)*UNIT_SIZE + 2] = matrix[(row * BLOCK_WIDTH + col)*UNIT_SIZE + 0]; // RGB -> BGR
+			frame[(base + row * FRAME_WIDTH + col)*UNIT_SIZE + 1] = matrix[(row * BLOCK_WIDTH + col)*UNIT_SIZE + 1];
+			frame[(base + row * FRAME_WIDTH + col)*UNIT_SIZE + 0] = matrix[(row * BLOCK_WIDTH + col)*UNIT_SIZE + 2];
+		}
+	}
+
+	if ((x + BLOCK_WIDTH) / FRAME_WIDTH) {
+		y += BLOCK_HEIGHT;
+		x = 0;
+	}
+	else {
+		x += BLOCK_WIDTH;
+	}
+
+	return 0;
+}
+
+int tranverse(uint8_t *frame)
+{
+	assert(NULL != frame);
+
+	uint8_t tmp;
+
+	for (int s_row = 0, d_row = FRAME_HEIGHT-1; s_row <= d_row; s_row++, d_row--) {
+		for (int col = 0; col < FRAME_WIDTH * 3; col++) {
+			tmp = frame[s_row * FRAME_WIDTH * 3 + col]; // TODO, buffer++, optimize time
+			frame[s_row * FRAME_WIDTH * 3 + col] = frame[d_row * FRAME_WIDTH * 3 + col];
+			frame[d_row * FRAME_WIDTH * 3 + col] = tmp;
+		}
+	}
 
 	return 0;
 }
@@ -27,9 +102,11 @@ int output(uint8_t *matrix)
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int ret;
+	FILE *fp;
 
-	ret = fopen_s(&fp, "output.rgb24", "wb");
+	ret = fopen_s(&fp, "output.rgb24.bmp", "wb");
 	assert(0 == ret);
+	assert(NULL != fp);
 
 	const char *filename = "test.jpg";
 	uint8_t *buffer = NULL;
@@ -72,7 +149,15 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 
-	assert(NULL != fp);
+	ret = write_bmp_header(fp);
+	assert(0 == ret);
+
+	ret = tranverse(frame);
+	assert(0 == ret);
+
+	ret = fwrite(frame, 1, FRAME_SIZE, fp);
+	assert(ret == FRAME_SIZE);
+	
 	ret = fclose(fp);
 	assert(0 == ret);
 
